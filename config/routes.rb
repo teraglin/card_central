62e89447f853c83f42749c4115b35e8f85ca69eb
teraglin <apkirvan@hotmail.com>
2021-02-11 16:05:42 +1100

    fixed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :listings
 
-  root :to 'listings#index'
+  root 'listings#index'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
